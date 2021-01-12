47a02d13b143191de319e0b942360c26b3fe6006
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-01-12 14:40:49 -0600

    Finished mouse over and mouse out event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,63 @@
 // Your code goes here
+//VARIABLES----------------------------------------------------
+let nav = document.querySelectorAll('header .nav-link');
+let busPic = document.querySelector('.intro img');
+console.log(busPic);
+
+busPic.style.filter = 'gracyscale(100%)';
+
+ 
+
+
+
+
+
+
+
+
+//EVENT LISTENERS ---------------------------------------------
+
+//mouse over
+nav[0].addEventListener('mouseover', mouseOverColor);
+nav[1].addEventListener('mouseover', mouseOverColor);
+nav[2].addEventListener('mouseover', mouseOverColor);
+nav[3].addEventListener('mouseover', mouseOverColor);
+
+//mouse out
+nav[0].addEventListener('mouseout', mouseOut);
+nav[1].addEventListener('mouseout', mouseOut);
+nav[2].addEventListener('mouseout', mouseOut);
+nav[3].addEventListener('mouseout',mouseOut);
+
+
+
+
+
+
+
+//FUNCTIONS---------------------------------------------------------
+
+//mouse over and out
+function mouseOverColor(e){
+    let elm = event.target;
+    elm.style.color = '#C1666B';
+    elm.style.background = '#494850';
+    elm.style.padding = '1rem';
+    elm.style.borderRadius = '1rem';
+    elm.style.fontWeight = 'Bold';
+}
+
+function mouseOut(e){
+    let elm = event.target;
+    elm.style.color = null;
+    elm.style.background = null;
+    elm.style.padding = null;
+    elm.style.borderRadius = null;
+    elm.style.fontWeight = null; 
+}
+
+//key down dark mode
+
+function grayScaleBus(e){
+
+}
